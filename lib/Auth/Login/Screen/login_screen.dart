ea8ea666ad17c04aa53aaa6a01e0f744061113f2
DeepakPc <developerdeepak01@gmail.com>
2021-11-22 12:01:35 +0530

    sizer done

---
+++ lib/Auth/Login/Screen/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Deepak", style: TextStyle(fontSize: 50.sp)),
+      ),
+    );
+  }
+}
